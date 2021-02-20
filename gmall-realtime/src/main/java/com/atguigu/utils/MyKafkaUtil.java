035ba917ffd61a517f9cf3f0d26d49031881b286
harry <harry@atguigu.com>
2021-02-20 17:01:03 +0800

    day02

---
+++ gmall-realtime/src/main/java/com/atguigu/utils/MyKafkaUtil.java
@@ -0,0 +1,38 @@
+package com.atguigu.utils;
+
+import org.apache.flink.api.common.serialization.SimpleStringSchema;
+import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer;
+import org.apache.flink.streaming.connectors.kafka.FlinkKafkaProducer;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+
+import java.util.Properties;
+
+public class MyKafkaUtil {
+
+    private static String KAFKA_SERVER = "hadoop102:9092,hadoop103:9092,hadoop104:9092";
+    private static Properties properties = new Properties();
+
+    static {
+        properties.setProperty("bootstrap.servers", KAFKA_SERVER);
+    }
+
+    /**
+     * 获取KafkaSource的方法
+     *
+     * @param topic   主题
+     * @param groupId 消费者组
+     */
+    public static FlinkKafkaConsumer<String> getKafkaSource(String topic, String groupId) {
+
+        //给配置信息对象添加配置项
+        properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG, groupId);
+
+        //获取KafkaSource
+        return new FlinkKafkaConsumer<String>(topic, new SimpleStringSchema(), properties);
+    }
+
+    public static FlinkKafkaProducer<String> getKafkaSink(String topic) {
+        return new FlinkKafkaProducer<String>(topic, new SimpleStringSchema(), properties);
+    }
+
+}
